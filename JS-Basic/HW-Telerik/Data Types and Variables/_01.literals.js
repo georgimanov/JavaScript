056cb7ef827650f4be986bdd3f3c7fa613177fd8
Geogri Manov <georgimanov@gmail.com>
2015-05-13 16:21:31 +0300

    [JS][HW] Data types and variables

---
+++ JS-Basic/HW-Telerik/Data Types and Variables/_01.literals.js
@@ -0,0 +1,23 @@
+var int = 5;
+console.log(typeof(int)); // Number: integer
+console.log(int); // 5
+
+var floatingPoint = 1.1;
+console.log(typeof(int)); // Number: Floating Point
+console.log(floatingPoint); // 1.1
+
+var booleanType = false;
+console.log(typeof(booleanType)); // Boolean
+console.log(booleanType); // false
+
+var stringType = 'Welcome to JavaScript';
+console.log(typeof(stringType)); // String
+console.log(stringType); // Welcome to JavaScript
+
+var nullable = null;
+console.log(typeof(nullable)); // Object
+console.log(nullable); // null
+
+var notDefined;
+console.log(typeof(notDefined)); // undefined
+console.log(notDefined); // undefined
---
+++ JS-Basic/HW-Telerik/Data Types and Variables/_04.nullAndUndefined.js
@@ -0,0 +1,7 @@
+var nullable = null;
+console.log(typeof(nullable)); // Object
+console.log(nullable); // null
+
+var notDefined;
+console.log(typeof(notDefined)); // undefined
+console.log(notDefined); // undefined
